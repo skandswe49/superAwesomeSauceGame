f28bcd56d561b21167a3fa1c786b523881524639
tired-cow <jean.pierre.manapsal@gmail.com>
2024-07-04 17:19:52 -0400

    Fixed collision detection

--- map.py
+++ map.py
@@ -1,15 +1,20 @@
 import vector2d
 
 class map:
-    def __init__(self, tile_width, map_array) -> None:
+    def __init__(self, tile_width : int, map_array : list[list[int]]) -> None:
         self.tile_width = tile_width
         self.map_array = map_array
 
     def is_colliding(self, position : vector2d):
-        position.x %= self.tile_width
-        position.y %= self.tile_width
+        x = int(position.x / self.tile_width)
+        y = int(position.y / self.tile_width)
 
-        if (self.map_array[position.y][position.x] == 0):
+        if ((x or y) < 0):
             return True
-        
-        return False
+        elif (x > len(self.map_array[0]) or y > len(self.map_array)):
+            return True
+
+        if (self.map_array[x][y] == 0):
+            return False
+        else:
+            return True
